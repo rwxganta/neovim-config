7f65c15642521e085d8de74cd4b5c2170927b59b
Luka Mendonça <lukamendoncadev@proton.me>
2024-06-24 21:28:48 -0300

    Update packages && identation fix

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,47 +1,44 @@
 return {
-	{
-		"williamboman/mason.nvim",
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require('mason').setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
 
-		config = function()
-			require('mason').setup()
-		end
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
+    config = function()
+        require('mason-lspconfig').setup({
+            ensure_installed = { 'lua_ls', 'html', 'cssls', 'cssmodules_ls', 'tsserver' }
+        })
+    end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local lspconfig = require('lspconfig')
+            lspconfig.lua_ls.setup({
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { 'vim', 'require' },
+                        }
+                    }
+                }
+            }) -- END lua_ls
 
-		config = function()
-			require('mason-lspconfig').setup({
-				ensure_installed = { 'lua_ls', 'html', 'cssls', 'cssmodules_ls', 'tsserver' }
-			})
-		end
-	},
-	{
-		"neovim/nvim-lspconfig",
-
-		config = function()
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
-			local lspconfig = require('lspconfig')
-
-			lspconfig.lua_ls.setup({
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { 'vim', 'require' },
-						}
-					}
-				}
-			}) -- END lua_ls
-
-			lspconfig.tsserver.setup({
-				capabilities = capabilities;
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities;
-			})
-			lspconfig.cssls.setup({
-				capabilities = capabilities;
-			})
-		end -- END CONFIG
-	}
+            lspconfig.tsserver.setup({
+                capabilities = capabilities;
+            })
+            lspconfig.html.setup({
+                capabilities = capabilities;
+            })
+            lspconfig.cssls.setup({
+                capabilities = capabilities;
+            })
+        end -- END config
+    }
 }
 
